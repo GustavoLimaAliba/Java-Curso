cdeafde5cd15f05236ea62c355a1338d31e5b438
GustavoLimaAliba <gustavo.aliba@sptech.school>
2022-07-18 11:17:01 -0300

    adicionando variaveis

---
+++ JAVA/aula3/src/javabasico/variaveis/variaves.java
@@ -0,0 +1,28 @@
+package javabasico.variaveis;
+
+public class variaves {
+
+	public static void main(String[] args) {
+		byte numByte = 127; // -128 a 127 (8 bits)
+		short numShort = 12321; // -32768 a 32767 (16 bits)
+		int numInt = 2147483647; // - 2.147.483.648 a 2.147.483.647(32 bits)
+		long numLong = 897997888; // -9.223.372.036.854.770.000 a 9.223.372.036.854.770.000 (64 bits)
+		
+		
+		float numFloat = 10.000f; // -1.4024E-37 a 3.40282347E+38 (32BITS)
+		double numDouble = 1000000000.000; // -4,94E-307 A 1.79769313486231570E+308 (64 BITS)
+		
+		
+		char varChar = 'F'; //0 A 65535 (16 BITS)
+		boolean varBoolean = true; // true or false
+		
+		
+		
+		
+		String texto ="bem vindos Devs";
+		Integer numeroConta = 4000;
+		Long numeroLongo = 1000L;
+		
+		String nome = "Gustavo";
+	}
+}
---
+++ JAVA/aula3/src/javabasico/variaveis/vetor.java
@@ -0,0 +1,5 @@
+package javabasico.variaveis;
+
+public class vetor {
+
+}
