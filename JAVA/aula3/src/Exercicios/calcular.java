fcafcb7517f1c6ab1445ec7f49ff931380818d38
GustavoLimaAliba <gustavo.aliba@sptech.school>
2022-07-22 13:21:44 -0300

    Aula 01 - Alura

---
+++ JAVA/aula3/src/Exercicios/calcular.java
@@ -0,0 +1,27 @@
+package Exercicios;
+
+public class calcular {
+double peso;
+double altura;
+int idade;
+
+private double getPeso() {
+
+	return peso;
+}
+private void setPeso(double peso) {
+	this.peso = peso;
+}
+private double getAltura() {
+	return altura;
+}
+private void setAltura(double altura) {
+	this.altura = altura;
+}
+private int getIdade() {
+	return idade;
+}
+private void setIdade(int idade) {
+	this.idade = idade;
+}
+}
---
+++ JAVA/aula3/src/Exercicios/calcularImc.java
@@ -0,0 +1,13 @@
+package Exercicios;
+import java.util.*;
+public class calcularImc {
+
+	public static void main(String[] args) {
+		Scanner peso = new Scanner(System.in);
+		
+		Double peso1 = peso.nextDouble();
+		
+
+	}
+
+}
